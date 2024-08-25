7f9f153c3b1fd7aeca2218c9d7fafca2d2dc9531
Wall'e <duanqingbb@163.com>
2024-08-25 14:11:58 +0800

    add adding and deleting doc demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { SetStateAction, useDeferredValue, useEffect, useState } from 'react'
 import './App.css'
 import { initializeApp } from 'firebase/app'
-import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore } from 'firebase/firestore'
 
-type Book  = {
-  id: string
+type Book = {
+  id?: string
   author: string
   title: string
 }
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAQoBjqsQbm-0Zw7sCxpEtb8sgj31hXWnQ",
+//   authDomain: "fir-9-c149d.firebaseapp.com",
+//   projectId: "fir-9-c149d",
+//   storageBucket: "fir-9-c149d.appspot.com",
+//   messagingSenderId: "315942202177",
+//   appId: "1:315942202177:web:77217bd16604c570470fc0",
+//   measurementId: "G-7Q0BDL0GH8"
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyAQoBjqsQbm-0Zw7sCxpEtb8sgj31hXWnQ",
-    authDomain: "fir-9-c149d.firebaseapp.com",
-    projectId: "fir-9-c149d",
-    storageBucket: "fir-9-c149d.appspot.com",
-    messagingSenderId: "315942202177",
-    appId: "1:315942202177:web:77217bd16604c570470fc0",
-    measurementId: "G-7Q0BDL0GH8"
+  apiKey: "AIzaSyDsooB17qabx4nXTU3dFyjYknT9gVJRjuw",
+  authDomain: "fir-9-2-2b858.firebaseapp.com",
+  projectId: "fir-9-2-2b858",
+  storageBucket: "fir-9-2-2b858.appspot.com",
+  messagingSenderId: "868628130158",
+  appId: "1:868628130158:web:960f66da4679065244397f",
+  measurementId: "G-YW6WBE7ET8"
 };
 
 //init firebase app
@@ -28,44 +35,109 @@ const db = getFirestore()
 //collection ref
 const colRef = collection(db, 'books')
 
-getDocs(colRef).then((snapshot) => {
-  let books: Book[] = []
-  snapshot.docs.forEach((doc) => {
-    books.push({ ...(doc.data() as Book)})
+async function addBook(book: Partial<Book>): Promise<void> {
+  const _ = await addDoc(colRef, {
+    ...book
   })
-  console.log(books)
-}).catch(err => {
-    console.log(err.message)
-})
+  console.log("a book added")
+}
+async function delBook(id: string): Promise<void> {
+  const docRef = doc(db, "books", id)
+  await deleteDoc(docRef)
+}
 
+async function ListBooks(): Promise<Book[]> {
+  let books: Book[] = []
+  try {
+    const snapshot = await getDocs(colRef)
+    for (const book of snapshot.docs) {
+      books.push({ ...(book.data() as Book), id: book.id})
+    }
+  } catch (error) {
+    throw new Error(`failed to fetch books: ${error}`)
+  }
+  return books
+}
 
 
 function App() {
+  const [bookList, setBookList] = useState<Book[]>([])
 
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    ListBooks().then((books: Book[]) => {
+      setBookList(books)
+    }).catch((e) => {
+      console.error(e)
+    })
+  }, [])
+  const bookListComp = bookList.length < 1 ? (<></>) : bookList.map(item => {
+    return (
+      <div className='card book' key={item.id} >
+        <span className='book-author'>
+          <span className='prefix'>Author</span>
+          <span> {item.author} </span>
+
+        </span>
+        <span className='book-title'>
+          <span className='prefix'>Title</span>
+          <span> {item.title} </span>
+        </span>
+        <button onClick={(e) => {
+          const bookId = item.id
+          console.log(item)
+          console.log(bookId)
+          delBook(bookId!).then(() => {
+            console.log(`book ${bookId} deleted`)
+          }).catch((e) => {
+            console.log(`failed to delete ${bookId} (${e})`)
+          })
+        }}>Delete</button>
+      </div>
+    )
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='c-app'>
+        <h1>Firebase Firestore</h1>
+
+        <div className='c-book-list'>
+          <h3>Book Shelf</h3>
+          {bookListComp}
+        </div>
+
+        <div className="c-add-book">
+          <form className='card' onSubmit={(e) => {
+            e.preventDefault()
+            const addBookForm = e.target as HTMLFormElement
+            const title = addBookForm.title.value
+            const author = addBookForm.author.value
+            addBook({
+              title: title,
+              author: author,
+            })
+          }}>
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" required />
+            <label htmlFor="author">Author</label>
+            <input type="text" name="author" required />
+            <button>add a new book</button>
+          </form>
+        </div>
+
+        <div className='c-delete-book'>
+          <form className="card" onSubmit={(e) => {
+            e.preventDefault()
+            const form = e.target as HTMLFormElement
+            const bookId = form.id.value
+
+          }}>
+            <label htmlFor="id">Document id:</label>
+            <input type="text" name="id" required />
+            <button>delete a book</button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
